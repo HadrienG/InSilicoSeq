ac8ef0fd729593d762c8b9bd566bc58d5ddfca03
Hadrien Gourlé <gourlehadrien@gmail.com>
2016-12-13 09:43:09 +0100

    singleton problem

--- iss/bam.py
+++ iss/bam.py
@@ -30,19 +30,31 @@ def parse_bam(bam_file):
     with pysam.AlignmentFile(bam_file, "rb") as bam:
         # print(dir(bam))
         for read in bam.fetch():
-            # print(dir(read))
             # print(read.is_paired)
             if not read.is_unmapped:
-                md_tag = read.get_aligned_pairs(
+                alignment = read.get_aligned_pairs(
                     matches_only=True,
                     with_seq=True
                     )
-                for base in md_tag:
+                for base in alignment:
                     if read.seq[base[0]] != 'N':
                         query_pos = base[0]
                         ref_base = base[2]
                         query_base = read.seq[query_pos]
                         dispatch_key = ref_base + query_base
-                        print(read.is_reverse, read.mate_is_reverse)
+                        # print(read.tags)  # must be smarter to find MD tag
+                        md_index = (i for i, v in enumerate(
+                            read.tags) if v[0] == 'MD')
+                        print(read.tags[next(md_index)])
+                        print(read.tags)
+                        print(read.is_paired)
+                        if '^' in read.tags[6][1]:
+
+                            # if dispatch_key == 'TA':
+                            #     print(read.tags, dir(read))
+                            continue
+                            # how to handle deletions?
+                            # cannot base ourselves of positions
+                            # since you get a deletion, the bases are off :(
                         array[query_pos, dispatch_dict[dispatch_key]] += 1
     print(array)
