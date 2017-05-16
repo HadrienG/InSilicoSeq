18209d589eb868f6e5b296ea3fdf52c8940f481e
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-05-16 15:09:41 +0200

    added test_util

---
+++ iss/test/test_util.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from iss import util
+
+
+def test_phred_conversions():
+    assert util.phred_to_prob(40) == 0.9999
+    assert util.phred_to_prob(30) == 0.999
+    assert util.phred_to_prob(20) == 0.99
+    assert util.phred_to_prob(10) == 0.9
+
+    assert util.prob_to_phred(0.9999) == 40
+    assert util.prob_to_phred(0.999) == 30
+    assert util.prob_to_phred(0.99) == 20
+    assert util.prob_to_phred(0.9) == 10
+
+
+def test_rev_comp():
+    lowercase_seq = 'attgctat'
+    uppercase_seq = 'CCGATTAC'
+    assert util.rev_comp(lowercase_seq) == 'atagcaat'
+    assert util.rev_comp(uppercase_seq) == 'GTAATCGG'
