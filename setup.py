e3b219f3a23da1082370b5e877d6db029dc0df2f
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-05-17 15:11:42 +0200

    added setup.py, modif cov tests

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='InSilicoSeq',
+    version='0.1.0',
+
+    description='a sequencing simulator',
+
+    url='https://github.com/HadrienG/InSilicoSeq',
+    download_url='https://github.com/HadrienG/InSilicoSeq/tarball/0.1.0',
+    author='Hadrien Gourlé',
+    author_email='hadrien.gourle@slu.se',
+
+    license='MIT',
+    packages=find_packages(),
+
+    tests_require=['nose'],
+    install_requires=['numpy', 'scipy', 'biopython', 'pysam'],
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': ['iss = iss.app:main'],
+    }
+)
