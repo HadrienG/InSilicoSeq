c33ef92dacc6e24b2ac1c0050c432952fc7baf58
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-05-17 13:47:56 +0200

    test phred scores

--- iss/test/test_error_model.py
+++ iss/test/test_error_model.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from iss.error_models import ErrorModel, basic, cdf
+from iss.error_models import ErrorModel, basic, cdf, kde
 
 import numpy as np
 
 
-def test_basic_error_model():
+def test_basic_phred():
     np.random.seed(42)
     err_mod = basic.BasicErrorModel()
 
-    distribution = err_mod.gen_phred_scores(0.99)[:10]
+    distribution = err_mod.gen_phred_scores(20)[:10]
     assert distribution == [23, 19, 25, 40, 19, 19, 40, 26, 18, 23]
+
+
+def test_cdf_phred():
+    np.random.seed(42)
+    err_mod = cdf.CDFErrorModel('data/ecoli_cdf.npz')
+    distribution = err_mod.gen_phred_scores(err_mod.quality_reverse)[:10]
+    assert distribution == [11, 21, 40, 40, 31, 31, 31, 40, 40, 40]
+
+
+def test_kde_phred():
+    np.random.seed(42)
+    err_mod = kde.KDErrorModel('data/ecoli_kde.npz')
+    distribution = err_mod.gen_phred_scores(err_mod.quality_reverse)[:10]
+    assert distribution == [10, 20, 40, 40, 30, 30, 30, 40, 40, 40]
