8ecb2abc0935205bbd948d79e55c3e91d1d4d13b
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-09-21 15:54:34 +0200

    catch indexerror in indel

--- iss/error_models/__init__.py
+++ iss/error_models/__init__.py
@@ -91,7 +91,7 @@ class ErrorModel(object):
         quality_list = record.letter_annotations["phred_quality"]
         position = 0
         for nucl, qual in zip(mutable_seq, quality_list):
-            if random.random() > util.phred_to_prob(qual):
+            if random.random() > util.phred_to_prob(qual) and nucl not in 'RYWSMKHBVDN':
                 mutable_seq[position] = str(np.random.choice(
                     nucl_choices[position][nucl][0],
                     p=nucl_choices[position][nucl][1]))
@@ -169,16 +169,20 @@ class ErrorModel(object):
         mutable_seq = record.seq.tomutable()
         position = 0
         for nucl in range(self.read_length - 1):
-            if mutable_seq[nucl] in 'RYWSMKHBVDN':
+            try:
+                # skip ambiguous nucleotides
+                if mutable_seq[nucl] in 'RYWSMKHBVDN':
+                    position += 1
+                    continue
+                for nucl_to_insert, prob in insertions[position].items():
+                    if random.random() < prob:
+                        # we want to insert after the base read
+                        mutable_seq.insert(position + 1, nucl_to_insert)
+                if random.random() < deletions[position][mutable_seq[nucl]]:
+                    mutable_seq.pop(position)
                 position += 1
+            except IndexError as e:
                 continue
-            for nucl_to_insert, prob in insertions[position].items():  # ins
-                if random.random() < prob:
-                    # we want to insert after the base read, hence position + 1
-                    mutable_seq.insert(position + 1, nucl_to_insert)
-            if random.random() < deletions[position][mutable_seq[nucl]]:  # del
-                mutable_seq.pop(position)
-            position += 1
 
         seq = self.adjust_seq_length(
             mutable_seq, orientation, full_seq, bounds)
