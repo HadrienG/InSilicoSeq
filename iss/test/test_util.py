56d14487126cf8520fdd6f65f13fbec4d8c0d7dd
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-10-17 08:53:54 +0200

    added split_list

--- iss/test/test_util.py
+++ iss/test/test_util.py
@@ -29,3 +29,29 @@ def test_count_records():
         record_list = util.count_records(f)
     assert record_list == [
         'genome_A', 'genome_T', 'genome_GC', 'genome_ATCG', 'genome_TA']
+
+
+def test_split_list():
+    test_range = range(10)
+    test_list = list(range(10))
+    # tests on range
+    assert util.split_list(test_range) == [range(10)]
+    assert util.split_list(test_range, n_parts=2) == [
+        range(0, 5),
+        range(5, 10)
+    ]
+    assert util.split_list(test_range, n_parts=3) == [
+        range(0, 3),
+        range(3, 6),
+        range(6, 10)
+    ]
+    # tests on lists
+    assert util.split_list(test_list) == [list(range(10))]
+    assert util.split_list(test_list, n_parts=2) == [
+        list(range(0, 5)),
+        list(range(5, 10))]
+    assert util.split_list(test_list, n_parts=3) == [
+        list(range(0, 3)),
+        list(range(3, 6)),
+        list(range(6, 10))
+    ]
--- iss/util.py
+++ iss/util.py
@@ -84,3 +84,18 @@ def count_records(fasta_file):
         sys.exit(1)
     else:
         return record_list
+
+
+def split_list(l, n_parts=1):
+    """Split a list in a number of parts
+
+    Args:
+        l (list): a list
+        n_parts (in): the number of parts to split the list in
+
+    Returns:
+        list: a list of n_parts lists
+    """
+    length = len(l)
+    return [l[i * length // n_parts: (i + 1) * length // n_parts]
+            for i in range(n_parts)]
