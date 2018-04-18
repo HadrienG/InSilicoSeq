3612180fb398fe4a07a511991b8762b78f54933b
Hadrien Gourlé <gourlehadrien@gmail.com>
2018-04-18 21:10:22 +0200

    ran iss + wrote bear sh wrapper

---
+++ benchmarks/README.md
@@ -0,0 +1,45 @@
+# Benchmarks
+
+## Introduction
+
+20 random genomes, selected with
+
+```bash
+# only 10k reads because we only care about the genomes and mode basic (faster)
+iss generate --ncbi bacteria viruses archaea -u 10 5 5 -n 10k --mode basic \
+    --cpus 8 --output _reads
+mv _reads_genomes.fasta test_genomes.fasta
+```
+
+## Software used in this benchmarks
+
+### Simulators
+
+See #49
+
+### Other
+
+* GNU time (1.9)
+
+## Commands used
+
+### iss
+
+```bash
+env time -v iss generate --genomes test_genomes.fasta -n 0.5m --model miseq --cpus 4 --output iss/test_x
+```
+
+| run | time (m:s) | memory (kb) |
+| --- | --- | --- |
+| 1 | 8:19.88 | 335100 |
+| 2 | 7:14.63 | 312708 |
+| 3 | 8:38.22 | 439920 |
+| 4 | 8:36.25 | 360900 |
+| 5 | 9:05.43 | 700648 |
+| 6 | 8:12.44 | 307652 |
+| 7 | 7:44.63 | 394100 |
+| 8 | 8:51.43 | 530440 |
+| 9 | 8:31.71 | 305276 |
+| 10 | 8:31.07 | 299068 |
+
+## BEAR
---
+++ benchmarks/run_bear.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+usage="$(basename "${0}") [-h] [-g fasta] [-r reads] [-o output] --
+        Wrapper script around BEAR
+
+        options:
+    		-g <multifasta> reference genomes in fasta format
+            -r <reads> read file in fastq format
+            -o <str> prefix for output files"
+
+
+while getopts "hg:r:o:" option
+    do
+        case "${option}" in
+            h) echo "${usage}"
+            exit 0
+                ;;
+            g) reference=$(readlink -f "${OPTARG}")
+                ;;
+            r) reads=$(readlink -f "${OPTARG}")
+                ;;
+            o) output="${OPTARG}"
+                ;;
+            :) printf "missing argument for -%s\n" "${OPTARG}" >&2
+            echo "${usage}" >&2
+            exit 1
+                ;;
+            \?) printf "illegal option -%s\n" "${OPTARG}" >&2
+            echo "${usage}">&2
+            exit 1
+                ;;
+        esac
+    done
+shift $((OPTIND-1))
+
+module load bear
+
+parametric abundance.pl "${reference}" med "${output}.txt"
+drisee.py --percent -n 3 -b 10000 -x 10000 -t fastq -s 500000 "${reads}" \
+    "${output}_err"
+error quality.pl "${reads}" "${output}_err.uc"
+generate_reads.py -r "${reference}" -a "${output}.txt" -o "${output}.fastq" \
+    -t 500000 -l 300 -i 350 -s 10
+trim_reads.py -i "${reads}" -f "${reference}" -o "${output}.fastq" -r \
+    "${output}_err.per" -q "${output}_err.err.qual" -m "${output}_err.err.matr"
