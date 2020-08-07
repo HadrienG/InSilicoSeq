610c0c3eab73904c144325be179b69230078a465
Hadrien Gourlé <gourlehadrien@gmail.com>
2020-08-07 16:01:06 +0200

    dont raise AttrError (fix #177)

--- iss/app.py
+++ iss/app.py
@@ -573,4 +573,4 @@ def main():
         logger = logging.getLogger(__name__)
         logger.debug(e)
         parser.print_help()
-        raise  # extra traceback to uncomment if all hell breaks lose
+        # raise  # extra traceback to uncomment if all hell breaks lose
